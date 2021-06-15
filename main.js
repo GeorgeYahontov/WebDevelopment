8010db52b9f9e05439a9c01d98be28d966faad70
George <yaxontov1995@gmail.com>
2021-06-15 21:27:29 +0300

    Homework 2

--- main.js
+++ main.js
@@ -1,78 +1,133 @@
-
-var myNum = 10;
-var myStr = 'строка';
-var myBool = true;
-var myArr = [1, 2, 3, 4, 5];
-var myObj = {
-    first: 'First Name',
-    last: 'Last Name'
+var userObj = {
+    age: 20,
+    firstName: 'Name',
+    lastName: 'Last',
+    fullName() {
+        return `${userObj.firstName} ${userObj.lastName}`;
+    }
 };
+console.log(userObj);
+console.log(userObj.fullName());
+function defUpperStr(str) {
+    return (str || 'Default text').toUpperCase();
+}
+console.log(defUpperStr('My text'));
+console.log(defUpperStr());
+function evenFn(n) {
+    let arr = [];
+    for (let i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
+    return arr;
+}
+console.log(evenFn(10));
+console.log(evenFn(15));
+console.log(evenFn(20));
 
-console.log('myNum: ', myNum);
-console.log('myStr:', myStr);
-console.log('myBool:', myBool);
-console.log('myArr:', myArr);
-console.log('myObj:', myObj);
-
-
-var decimal2 = myNum.toFixed(2);
-
-var i = 0;
-
-console.log('Префиксный инкремент:', ++i);
-console.log('Постфиксный инкремент:', i++);
-console.log('Значение i:', i);
-console.log('Префиксный декремент:', --i);
-console.log('Постфиксный декремент:', i--);
-console.log('Значение i:', i);
-
-var myTest = 20;
-
-myTest += myNum;
-console.log('+=', myTest);
-
-myTest -= 10;
-console.log('-=', myTest);
-
-myTest *= 2;
-console.log('*=', myTest);
-
-myTest /= myNum;
-console.log('/=', myTest);
-
-myTest %= 3;
-console.log('%=', myTest);
-
-var myPi = Math.PI; // константа Pi → myPi
-console.log('myPi:', myPi);
-
-var myRound = Math.round(89.279); // округленное значение числа 89.279 → myRound
-console.log('myRound:', myRound);
-
-var myRandom = Math.random() * 10; // случайное число между 0..10 → myRandom
-console.log('myRandom:', myRandom);
-
-var myPow = Math.pow(3, 5); // 3 в 5 степени → myPow
-console.log('myPow:', myPow);
-
-
-var strObj = {str: 'Мама мыла раму, рама мыла маму'};
-
-strObj.length = strObj.str.length;
-console.log('strObj = ', strObj);
+function weekFn(cond) {
+    let str = '';
+    switch (cond) {
+        case 1:
+            str = 'Понедельник';
+            break;
+        case 2:
+            str = 'Вторник';
+            break;
+        case 3:
+            str = 'Среда';
+            break;
+        case 4:
+            str = 'Четверг';
+            break;
+        case 5:
+            str = 'Пятница';
+            break;
+        case 6:
+            str = 'Суббота';
+            break;
+        case 7:
+            str = 'Воскресенье';
+            break;
+        default:
+            str = null;
+    }
+    return str;
+}
+console.log(weekFn(1));
+console.log(weekFn(3));
+console.log(weekFn(7));
+console.log(weekFn(9));
+console.log(weekFn(1.5));
+console.log(weekFn('2'));
 
 
-var isRamaPos = strObj.str.indexOf('рама');
+function ageClassification(num) {
+    return num > 0
+        ? num > 24
+            ? num > 44
+                ? num > 65
+                    ? num > 75
+                        ? num > 90
+                            ? num > 122
+                                ? null
+                                : 'долгожители'
+                            : 'старческий возраст'
+                        : 'пожилой возраст'
+                    : 'средний возраст'
+                : 'молодой возраст'
+            : 'детский возраст'
+        : null;
+}
+console.log('    -1 :', ageClassification(-1));
+console.log('     1 :', ageClassification(1));
+console.log('    24 :', ageClassification(24));
+console.log(' 24.01 :', ageClassification(24.01));
+console.log('    44 :', ageClassification(44));
+console.log(' 44.01 :', ageClassification(44.01));
+console.log('    65 :', ageClassification(65));
+console.log('  65.1 :', ageClassification(65.1));
+console.log('    75 :', ageClassification(75));
+console.log(' 75.01 :', ageClassification(75.01));
+console.log('    90 :', ageClassification(90));
+console.log(' 90.01 :', ageClassification(90.01));
+console.log('   122 :', ageClassification(122));
+console.log('122.01 :', ageClassification(122.01));
+console.log('   150 :', ageClassification(150));
 
-console.log('isRamaPos:', isRamaPos);
 
 
-var strReplace = strObj.str.replace('мыла', 'моет');
+function oddFn(n) {
+    let arr = [];
+    let i = 0;
 
-strReplace = strReplace.replace('рама', 'Рама');
-strReplace = strReplace.replace('мыла', 'держит');
-console.log('strReplace:', strReplace);
+    while (i++ < n) if (i % 2 !== 0) arr.push(i);
+
+    return arr;
+}
+
+console.log(oddFn(10));
+console.log(oddFn(15));
+console.log(oddFn(20));
 
 
-console.log(myStr.toUpperCase());
-console.log('Любая Строка'.toLowerCase());
+function mainFunc(a, b, cb) {
+    if (cb && typeof cb === 'function') return cb(a, b);
+
+    return false;
+}
+
+function cbRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function cbPow(num, pow) {
+    return Math.pow(num, pow);
+}
+
+function cbAdd(a, b) {
+    return a + b;
+}
+
+
+console.log(mainFunc(2, 5, cbRandom));
+console.log(mainFunc(2, 5, cbPow));
+console.log(mainFunc(2, 5, cbAdd));
+console.log(mainFunc(2, 5, 'not a func'));
